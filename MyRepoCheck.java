640e221db3c43e0947baa8a26c66d6ea4ba28707
Rizwanalam965 <154456971+Rizwanalam965@users.noreply.github.com>
2024-10-26 22:21:47 +0530

    My third Change

--- MyRepoCheck.java
+++ MyRepoCheck.java
@@ -1,5 +1,6 @@
 public class MyRepoCheck {
     public static void main(String[] args) {
         System.out.println("Project Uploaded Successfully");
+        System.out.println("Added in Github Edited Mode");
     }
 }
